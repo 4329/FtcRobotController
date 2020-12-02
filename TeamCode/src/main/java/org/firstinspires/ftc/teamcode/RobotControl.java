9d1103bd3f397b89b65496ecef09a2dd38c85c8b
RoboteerCadet <roboteercadets@gmail.com>
2020-12-01 19:51:31 -0600

    We programmed our first try at autonomous mode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotControl.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class RobotControl {
+
+    /* Public OpMode members. */
+    public DcMotor leftFrontDrive = null;
+    public DcMotor rightFrontDrive = null;
+    public DcMotor leftBackDrive = null;
+    public DcMotor rightBackDrive = null;
+
+
+    /* local OpMode members. */
+    HardwareMap hardwareMap =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hardwareMap = ahwMap;
+
+        // Define and Initialize Motors
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        // Set all motors to zero power
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
+//
+//        // Set all motors to run without encoders.
+//        // May want to use RUN_USING_ENCODERS if encoders are installed.
+//        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        leftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//
+    }
+}
+
